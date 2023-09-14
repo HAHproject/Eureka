4084d3ad5c7ea7844b12fec3f5301b061321df18
cos12378 <yrias8700@gmail.com>
2023-09-14 17:11:01 +0900

    init

---
+++ src/test/java/com/example/reservationeureka/ReservationEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservationeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
